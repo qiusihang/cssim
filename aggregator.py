bugs (performance calculation) fixed, add an aggregator for road network

aggregator.py
@@ -1,19 +1,22 @@
 import numpy as np
 import latlng
+import treefinder
 from matplotlib import pyplot as plt
 
 class Aggregator:
 
-    def __init__(self):
+    def __init__(self, density_calculation_radius = 2.5, tree_min_distance = 5):
         self.objects = []
         self.weights = []
         self.aggregated_objects = []
+        self.threshold_1 = density_calculation_radius
+        self.threshold_2 = tree_min_distance
 
     def add_object(self, lat, lng, weight = 1):
         self.objects.append(latlng.LatLng(lat, lng))
         self.weights.append(weight)
 
-    def aggregate(self, threshold_1 = 2.5, threshold_2 = 5):
+    def aggregate(self):
     # threshold_1: radius for calculating density, threshold_2: min dis between trees
         num = len(self.objects)
         px = np.zeros(num)
@@ -28,16 +31,16 @@ class Aggregator:
             py[i] = xy.y
         for i in range(num):
             s = self.weights[i]
-            t = 1
+            #t = 1
             ave_x = px[i] * self.weights[i]
             ave_y = py[i] * self.weights[i]
             for j in range(num):
-                if i!=j and (px[i]-px[j])**2 + (py[i]-py[j])**2 < threshold_1 **2:
-                    t += 1
+                if i!=j and (px[i]-px[j])**2 + (py[i]-py[j])**2 < self.threshold_1 **2:
+                    #t += 1
                     s += self.weights[j]
                     ave_x += px[j] * self.weights[j]
                     ave_y += py[j] * self.weights[j]
-            rho[i] = t + np.random.rand()*0.01
+            rho[i] = s + np.random.rand()*0.01
             ll = self.objects[0].get_latlng(ave_x/s, ave_y/s)
             alat[i] = ll.lat
             alng[i] = ll.lng
@@ -53,8 +56,47 @@ class Aggregator:
 
         self.aggregated_objects = []
         for i in range(num):
-            if dis[i] > threshold_2**2:
+            if dis[i] > self.threshold_2**2:
                 self.aggregated_objects.append(latlng.LatLng(alat[i],alng[i]))
+
+    def aggregate_using_treefinder(self):
+    # threshold_1: radius for calculating density, threshold_2: min dis between trees
+    # only use this function when there are too many trees...
+        num = len(self.objects)
+        if num < 1: return
+        objs = []
+        for i in range(num):
+            objs.append(treefinder.Tree(i, "", self.objects[i].lat, self.objects[i].lng))
+        self.object_finder = treefinder.TreeFinder(None, objs)
+        rho = np.zeros(num)
+        alat = np.zeros(num)
+        alng = np.zeros(num)
+        for i in range(num):
+            t = 0
+            s = 0 #self.weights[i]
+            ave_lat = 0 #self.objects[i].lat * self.weights[i]
+            ave_lng = 0 #self.objects[i].lng * self.weights[i]
+            nearby_objects = self.object_finder.find_trees(self.objects[i].lat, self.objects[i].lng, self.threshold_1)
+            for tree in nearby_objects:
+                t += 1
+                s += self.weights[tree.id]
+                ave_lat += tree.lat * self.weights[tree.id]
+                ave_lng += tree.lng * self.weights[tree.id]
+            rho[i] = t + np.random.rand()*0.99
+            alat[i] = ave_lat/s
+            alng[i] = ave_lng/s
+
+        self.aggregated_objects = []
+        for i in range(num):
+            nearby_objects = self.object_finder.find_trees(self.objects[i].lat, self.objects[i].lng, self.threshold_2)
+            flag = True
+            for tree in nearby_objects:
+                if rho[tree.id] > rho[i]:
+                    flag = False
+                    break
+            if flag:
+                self.aggregated_objects.append(latlng.LatLng(alat[i],alng[i]))
+
 
     def plot_original(self, holdon = False):
         num = len(self.objects)

simulator.py
@@ -20,6 +20,7 @@ class Simulator:
         self.agents = []
         self.simulation_id = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
         self.setting = settingparser.SettingParser(filename)
+        self.time = 0
 
         if not os.path.exists("output"):
             os.makedirs("output")
@@ -31,79 +32,79 @@ class Simulator:
         while True:
             self.propulse(self.setting.time_stamp)
             self.output()
-            if self.setting.wm.time > end_time:
+            if self.time > end_time:
                 break
 
     def propulse(self, steps = 1):
         new_agent_waiting_time = 0
         for i in range(steps):
-            self.setting.wm.time += 1
-            if new_agent_waiting_time < 1:
-                level = random.random()
-                threshold = 0
+            self.time += 1
+            if new_agent_waiting_time < 1: # create a new agent
+                p = random.random()
+                probability = 0
                 for i in range(len(self.setting.worker_level_distribution)):
-                    threshold += self.setting.worker_level_distribution[i]
-                    if i == len(self.setting.worker_level_distribution) - 1 and threshold < 1:
-                        threshold = 1
-                    if level <= threshold:
-                        agent = Agent(self.setting.wm.new_worker(i), self.setting.tf, \
-                        self.setting.worker_labelling_probability[i], self.setting.worker_labelling_error[i],self.setting.worker_labelling_time[i],\
-                        self.setting.worker_moving_distance[i], self.setting.worker_moving_time[i])
+                    probability += self.setting.worker_level_distribution[i]
+                    if i == len(self.setting.worker_level_distribution) - 1:
+                        probability = 1
+                    if p <= probability:
+                        agent = Agent(\
+                        self.setting.wm.new_worker(i, self.time),\
+                        self.setting.tf,\
+                        self.setting.worker_labelling_probability[i],\
+                        self.setting.worker_labelling_error[i],\
+                        self.setting.worker_labelling_time[i],\
+                        self.setting.worker_moving_distance[i],\
+                        self.setting.worker_moving_time[i])
                         break
                 if len(agent.worker.task.uoas) > 0: # task has uoa.. meaning still has task to do
                     self.agents.append(agent)
                     new_agent_waiting_time = np.random.poisson(self.setting.worker_arrival_interval,1)[0]
-                else:
+                else: # finshed
                     new_agent_waiting_time = 100000000
             else:
                 new_agent_waiting_time -= 1
+
             for agent in self.agents:
-                if agent.worker.wm.time - agent.worker.strat_time > self.setting.dropout_time or agent.status == 0:
-                    agent.worker.status = 1
+                if self.time - agent.worker.strat_time > self.setting.dropout_time or agent.status == 0:
+                    agent.worker.status = 1 # exit
                     self.agents.remove(agent)
                 agent.run()
 
     def output(self):
         for property in self.setting.output_properties:
             foldername = "output/"+self.simulation_id+"/"+property
-            recall_a = None
-            recall_p = None
-            precision_a = None
-            precision_p = None
-            acc_a = None
-            acc_p = None
             if property == "tree_cover":
                 if not os.path.exists(foldername):
                     os.makedirs(foldername)
-                self.setting.tf.calc_performance(self.setting.rn, foldername+"/"+str(self.setting.wm.time)+".txt")
+                self.setting.tf.calc_performance(self.setting.rn, foldername+"/"+str(self.time)+".txt")
             elif property == "cost":
                 if not os.path.exists(foldername):
                     os.makedirs(foldername)
-                f = open(foldername+"/"+str(self.setting.wm.time)+".txt","w")
+                f = open(foldername+"/"+str(self.time)+".txt","w")
                 s = sum([worker.workload for worker in self.setting.wm.workers])
                 f.write("cost = " + str(s*self.setting.payment_per_workload) + "\n")
                 f.close()
             elif property == "worker_statistics":
                 if not os.path.exists(foldername):
                     os.makedirs(foldername)
-                self.setting.wm.output_stat(foldername+"/"+str(self.setting.wm.time)+".txt")
+                self.setting.wm.output_stat(foldername+"/"+str(self.time)+".txt")
             elif property == "task_statistics":
                 if not os.path.exists(foldername):
                     os.makedirs(foldername)
-                self.setting.ta.output_stat(foldername+"/"+str(self.setting.wm.time)+".txt")
-            elif property == "UOA_statistic":
-                if not os.path.exists(foldername):
-                    os.makedirs(foldername)
+                self.setting.ta.output_stat(foldername+"/"+str(self.time)+".txt")
+            #elif property == "UOA_statistic":
+            #    if not os.path.exists(foldername):
+            #        os.makedirs(foldername)
             elif property == "image":
                 if not os.path.exists(foldername):
                     os.makedirs(foldername)
-                self.plot(foldername+"/"+str(self.setting.wm.time)+".png")
+                self.plot(foldername+"/"+str(self.time)+".png")
 
     def plot(self, filename = ''):
         self.setting.rn.plot_map(True)
+        self.setting.rn.aggregator.plot_aggregated(True)
         for road in self.setting.rn.roads:
             road.predictor.plot(True)
-            road.aggregator.plot_aggregated(True)
         if len(filename) > 0:
             plt.savefig(filename)
         else:
@@ -115,15 +116,15 @@ class Agent:
     def __init__(self, worker, tf, lp, le, lt, md, mt):
     #treefinder, probability of label or not, standard deviation of labeling error, labeling time,
     #moving distance, moving time
-        self.worker = worker
-        self.tf = tf
-        self.lp = lp
-        self.le = le
-        self.lt = lt
-        self.md = md
-        self.mt = mt
-        self.status = 1 # 0:finished, 1:moving, 2:looking&labeling
-        self.lasting_time = 0 # lasting time of current status
+        self.worker                 = worker
+        self.tree_finder            = tf
+        self.labeling_probability   = lp
+        self.labeling_error         = le
+        self.labeling_time          = lt
+        self.moving_distance        = md
+        self.moving_time            = mt
+        self.status                 = 1 # 0:finished, 1:moving, 2:looking&labeling
+        self.lasting_time           = 0 # lasting time of current status
 
     def run(self, steps = 1):
         for i in range(steps):
@@ -133,11 +134,11 @@ class Agent:
             # here, lasting_time = 0, start new motion
             if self.status == 1:
                 n = self.label()
-                self.lasting_time = self.lt * n
+                self.lasting_time = self.labeling_time * n
                 self.status = 2
             elif self.status == 2:
-                if self.worker.move(self.md) == True:
-                    self.lasting_time = self.mt
+                if self.worker.move(self.moving_distance) == True:
+                    self.lasting_time = self.moving_time
                     self.status = 1
                 else:
                     if self.worker.task_validation() > 0.9:
@@ -153,11 +154,11 @@ class Agent:
         ll = uoa.road.get_latlng(self.worker.cur_pos)
         plt.scatter(ll.lng, ll.lat, c='cyan', marker='o')
         # find three nearest trees
-        trees = self.tf.find_trees(ll.lat, ll.lng, 10)
+        trees = self.tree_finder.find_trees(ll.lat, ll.lng, 10)
         n = 0
         for tree in trees:
-            if random.random() < self.lp:
-                r = random.normalvariate(0,self.le)
+            if random.random() < self.labeling_probability:
+                r = random.normalvariate(0,self.labeling_error)
                 theta = random.random() * math.pi
                 ll_tree = latlng.LatLng(tree.lat, tree.lng).get_latlng(r*math.cos(theta),r*math.sin(theta))
                 self.worker.label(ll_tree.lat, ll_tree.lng)